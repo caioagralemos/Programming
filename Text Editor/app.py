ba63f4bade924d671568f1bb29d0ab97da7b6aa5
caioagralemos <caioagralemos@live.com>
2023-02-21 20:26:59 -0300

    extras

--- Text Editor/app.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-from tkinter import font
-
-root = Tk()
-root.title("Datilógrafo")
-root.geometry("1200x600")
-
-root.mainloop()
---
+++ decifrador/app.py
@@ -0,0 +1,8 @@
+i
+
+doe = 0
+
+while doe != "1" or doe != "2":
+    doe = input("press 1 to decode or 2 to decode: ")
+
+if doe == "1":
